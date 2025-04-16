abdfee3f30dce4215f1b51ddf309cf8aa7ca8b64
eduardo <eduardo.rod@gmail.com>
2025-04-16 04:44:28 -0600

    Simplificar

--- 97.other.tf
+++
@@ -1,7 +0,0 @@
-####Para Proxmox
-resource "proxmox_virtual_environment_download_file" "debian-12-standard_lxc_img" {
-  content_type = "vztmpl"
-  datastore_id = "local"
-  node_name    = "vdc2-2"
-  url          = "http://download.proxmox.com/images/system/debian-12-standard_12.7-1_amd64.tar.zst"
-}
